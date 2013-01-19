5aaf79203dae57013fd086ac4ba67ae95c77da43
Brian Nguyen <bsnguyen@slugmail.ucsc.edu>
2013-01-19 14:02:10 -0800

    Added "pushables"

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -7,6 +7,8 @@ public class LevelManager : MonoBehaviour {
 	public PlayerController playerCharacter;
 	public InteractionManager interactionManager;
 	
+	public TimeSwitchObject[] timeSwitchObjects;
+	
 	//Make these private later: Also assign these targets based off prior choices / dispositions
 	public Transform youngSectionTarget;
 	public Transform middleSectionTarget;
@@ -47,6 +49,7 @@ public class LevelManager : MonoBehaviour {
 	}
 	
 	void HandleInput(){
+		/*
 		if(Input.GetButtonDown(Strings.ButtonYoungAge)){
 			ShiftToAge(PlayerController.CharacterAgeState.YOUNG, youngSectionTarget.position);
 		}
@@ -55,7 +58,81 @@ public class LevelManager : MonoBehaviour {
 		}
 		else if(Input.GetButtonDown(Strings.ButtonOldAge)){
 			ShiftToAge(PlayerController.CharacterAgeState.OLD, oldSectionTarget.position);
+		}*/
+		
+		if(Input.GetButtonDown(Strings.ButtonAgeShiftDown)){
+			ShiftDownAge();
 		}
+		else if(Input.GetButtonDown(Strings.ButtonAgeShiftUp)){
+			ShiftUpAge();
+		}
+		
+	}
+	
+	void ShiftUpAge(){
+		switch(playerCharacter.currentCharacterAge){
+			case PlayerController.CharacterAgeState.YOUNG:
+				//Switch to middle
+				ShiftToAge(PlayerController.CharacterAgeState.MIDDLE, middleSectionTarget.position);
+				foreach(TimeSwitchObject tsObject in timeSwitchObjects){
+					if(!tsObject.staticInYoung){
+						tsObject.middleTimeObject.transform.localPosition = tsObject.youngTimeObject.transform.localPosition;
+					}
+				}
+				break;
+			case PlayerController.CharacterAgeState.MIDDLE:
+				//Switch to Old
+				ShiftToAge(PlayerController.CharacterAgeState.OLD, oldSectionTarget.position);
+				foreach(TimeSwitchObject tsObject in timeSwitchObjects){
+					if(!tsObject.staticInMiddle){
+						tsObject.oldTimeObject.transform.localPosition = tsObject.middleTimeObject.transform.localPosition;
+					}
+				}
+				break;
+			case PlayerController.CharacterAgeState.OLD:
+				//Switch to Young
+				ShiftToAge(PlayerController.CharacterAgeState.YOUNG, youngSectionTarget.position);
+				break;
+		}
+	}
+	
+	void ShiftDownAge(){
+		switch(playerCharacter.currentCharacterAge){
+			case PlayerController.CharacterAgeState.YOUNG:
+				//Switch to Old
+				ShiftToAge(PlayerController.CharacterAgeState.OLD, oldSectionTarget.position);
+				foreach(TimeSwitchObject tsObject in timeSwitchObjects){
+					if(!tsObject.staticInYoung){
+						tsObject.oldTimeObject.transform.localPosition = tsObject.middleTimeObject.transform.localPosition;
+					}
+				}
+				break;
+			case PlayerController.CharacterAgeState.MIDDLE:
+				//Switch to Young
+				ShiftToAge(PlayerController.CharacterAgeState.YOUNG, youngSectionTarget.position);
+				break;
+			case PlayerController.CharacterAgeState.OLD:
+				//Switch to Middle
+				ShiftToAge(PlayerController.CharacterAgeState.MIDDLE, middleSectionTarget.position);
+				break;
+		}
+	}
+	
+	void ShiftToYoung(){
+		ShiftToAge(PlayerController.CharacterAgeState.YOUNG, youngSectionTarget.position);
+		foreach(TimeSwitchObject tsObject in timeSwitchObjects){
+			
+		}
+	}
+	
+	void ShiftToMiddle(){
+		ShiftToAge(PlayerController.CharacterAgeState.MIDDLE, middleSectionTarget.position);
+		
+	}
+	
+	void ShiftToOld(){
+		ShiftToAge(PlayerController.CharacterAgeState.OLD, oldSectionTarget.position);
+		
 	}
 	
 	void ShiftToAge(PlayerController.CharacterAgeState age, Vector3 frameOriginRelativeToWorld){
@@ -74,5 +151,9 @@ public class LevelManager : MonoBehaviour {
 		playerCharacter.transform.position = new Vector3(sectionPosRelativeToWorld.x + deltaPlayerToCurrentFrame.x,
 											sectionPosRelativeToWorld.y + deltaPlayerToCurrentFrame.y,
 											sectionPosRelativeToWorld.z);
+	}
+	
+	void GetTimeSwitchedPosition(Vector3 newSectionPosRelativeToWorld, Vector3 objectCurrentLocalPosition){
+		
 	}
 }
--- Assets/Scripts/Utility/Strings.cs
+++ Assets/Scripts/Utility/Strings.cs
@@ -4,6 +4,9 @@ public static class Strings{
 	public static string ButtonMiddleAge = "Age Middle";
 	public static string ButtonOldAge = "Age Old";
 	
+	public static string ButtonAgeShiftUp = "Age Shift Up";
+	public static string ButtonAgeShiftDown = "Age Shift Down";
+	
 	
 	public static string tag_Climbable = "Climbable";
 	public static string tag_Pushable = "Pushable";
