ce1415d55db115a3201c7c3a836e3240b1db0b23
ElBathroom <wadehribar@yahoo.com>
2013-02-15 03:02:29 -0800

    Added in all sound being defaulted as off, but can be toggled on when cheats are in the scene. NoClip seems to be impossible to do unless all collisions are turned off, since the character controller for the player can not be turned off.

--- assets/Scripts/Cheats/CheatListener.cs
+++ assets/Scripts/Cheats/CheatListener.cs
@@ -2,27 +2,33 @@ using UnityEngine;
 using System.Collections;
 
 public class CheatListener : MonoBehaviour {
+	public bool isFlying = false;
+	public bool isFastMovement = false;
+	public bool isNoClip = false;
+	public bool isSoundOff = true;
 	
-	public PlayerController playerController;
+	public float speedIncrease = 2;
 	
-	protected bool isFlying = false;
-	protected bool isFastMovement = false;
-	
-	protected float speedIncrease = 2;
+	protected PlayerController playerController;
+	protected LevelManager levelManager;
+	protected CharacterController characterController;
 	
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		playerController = gameObject.GetComponent<PlayerController>();
+		levelManager = gameObject.GetComponent<LevelManager>();
+		characterController = gameObject.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Fly ();
-		MoveFast ();
+	void Update() {
+		Fly();
+		MoveFast();
+		ToggleSound();
 	}
 	
 	// Toggle flying
-	void Fly () {
+	void Fly() {
 		if (isFlying){
 			playerController.isAffectedByGravity = false;
 		}
@@ -37,7 +43,7 @@ public class CheatListener : MonoBehaviour {
 	}
 	
 	// Toggle moving twice as fast
-	void MoveFast () {		
+	void MoveFast() {		
 		if (Input.GetKeyDown(KeyCode.M)) {
 			isFastMovement = !isFastMovement;
 			
@@ -45,6 +51,32 @@ public class CheatListener : MonoBehaviour {
 				playerController.walkSpeed = playerController.walkSpeed * speedIncrease;
 			} else {
 				playerController.walkSpeed = playerController.walkSpeed / speedIncrease;	
+			}
+		}	
+	}
+	
+	// Toggle noClip *Seems to be impossible unless all possible collisions are turned off for the player's character controller*
+	void NoClip() {		
+		if (Input.GetKeyDown(KeyCode.N)) {
+			isNoClip = !isNoClip;
+			
+			if (isNoClip) {
+				characterController.detectCollisions = false;
+			} else {
+				characterController.detectCollisions = true;	
+			}
+		}	
+	}
+	
+	// Toggle sound
+	void ToggleSound() {		
+		if (Input.GetKeyDown(KeyCode.A)) {
+			isSoundOff = !isSoundOff;
+			
+			if (isSoundOff) {
+				AudioListener.pause = true;
+			} else {
+				AudioListener.pause = false;
 			}
 		}	
 	}
