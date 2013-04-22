e6ddb8138f312849f999ff85a9e5175de050e464
inei <inei@ucsc.edu>
2013-04-22 14:13:58 -0700

    Redid the game object structure in the correct LevelBase.unity this time (this was the cause of the SFX crash errors early). Added a PlaySFX and a StopSFX functions to SoundManager.cs . Modified MoveState.cs to use the new functions.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -28,4 +28,47 @@ public class SoundManager : MonoBehaviour {
             return manager_instance;
         }
     }
+
+    /// <summary>
+    /// PlaySFX will play the sound effect that you specify. It is
+    /// primarily used for the walking sound effects.
+    /// </summary>
+    /// <param name="SFXName">The name of the walking sound effect
+    /// that should be played.</param>
+    public void PlaySFX(string SFXName)
+    {
+        switch (SFXName)
+        {
+            case "WalkForest":
+                SoundManager.instance.WalkForestSFX.Play();   
+                break;
+            case "WalkBeach":
+                break;
+            case "WalkPier":
+                break;
+            case "WalkMarket":
+                break;
+            case "WalkTree":
+                break;
+            case "WalkFarm":
+                break;
+            case "WalkLightHouse":
+                break;
+            case "WalkWindmill":
+                break;
+            case "WalkStairs":
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// StopSFX will stop all SFX that have been played using PlaySFX.
+    /// These are usually the walking sound effects for different areas.
+    /// </summary>
+    public void StopSFX()
+    {
+        SoundManager.instance.WalkForestSFX.Stop();
+    }
 }
