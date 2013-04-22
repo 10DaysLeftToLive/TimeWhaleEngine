5cfd1b59277f65d4e69fb9f734b2c92aeac97008
inei <inei@ucsc.edu>
2013-04-21 21:12:37 -0700

    Added in new BGM, took out old BGM, added in new SFX.  Moved the Music Game Object to the Sound Manager since it makes more sense over there.  Added fading down and up as 2 seperate functions for Crossfade.cs.  Added in a new AudioSource for SoundManager.cs that is the new SFX.  Added in a call to the SFX in MoveState.cs so that you can hear walking noises as you move about.  Added in a cheat for testing.

--- Assets/Scripts/Sound/Crossfade.cs
+++ Assets/Scripts/Sound/Crossfade.cs
@@ -4,100 +4,242 @@ using System.Collections.Generic;
 
 public class Crossfade: MonoBehaviour {	
 
-	public AudioSource ForestBGM;
-	public AudioSource BeachBGM;
-	public AudioSource CliffBGM;
-	public AudioSource LighthouseBGM;
-	public AudioSource TreeBGM;
-	public AudioSource MarketBGM;
+    public AudioSource ForestBGM;
+    public AudioSource ForestAmbient;
+    public AudioSource BeachBGM;
+    public AudioSource BeachAmbient;
+    public AudioSource FarmBGM;
+    public AudioSource FarmAmbient;
+    public AudioSource LighthouseBGM;
+    public AudioSource LighthouseAmbient;
+    public AudioSource WindmillBGM;
+    public AudioSource WindmillAmbient;
+    public AudioSource MarketBGM;
+    public AudioSource MarketAmbient;
 
-	public static void FadeBetween()
-	{
-		string Current = null;
-		string Next = null;
+    public static void FadeBetween()
+    {
+        string Current = null;
+        string Next = null;
 
-		Crossfade.CrossfadeInstance.StartCoroutine(CoroutineFadeOverTime(Current, Next));
-	}
+        Crossfade.CrossfadeInstance.StartCoroutine(CoroutineFadeOverTime(Current, Next));
+    }
 
-	static public Crossfade CrossfadeInstance;
-	
-	void Awake()
-	{
-		Crossfade.CrossfadeInstance = this;
-	}
+    static public Crossfade CrossfadeInstance;
+    
+    void Awake()
+    {
+        Crossfade.CrossfadeInstance = this;
+    }
 
-	public static IEnumerator CoroutineFadeOverTime(string Current, string Next)
-	{
+    public static IEnumerator CoroutineFadeDown(string Current)
+    {
+        AudioSource CurrentSong;
+        AudioSource CurrentAmbient;
+        float fTimeCounter = 0f;
 
-		AudioSource CurrentSong;
-		AudioSource NextSong;
+        switch (Current)
+        {
+            case "Forest":
+                CurrentSong = CrossfadeInstance.ForestBGM;
+                CurrentAmbient = CrossfadeInstance.ForestAmbient;
+                break;
+            case "Beach":
+                CurrentSong = CrossfadeInstance.BeachBGM;
+                CurrentAmbient = CrossfadeInstance.BeachAmbient;
+                break;
+            case "Cliff":
+                CurrentSong = CrossfadeInstance.FarmBGM;
+                CurrentAmbient = CrossfadeInstance.FarmAmbient;
+                break;
+            case "Light":
+                CurrentSong = CrossfadeInstance.LighthouseBGM;
+                CurrentAmbient = CrossfadeInstance.LighthouseAmbient;
+                break;
+            case "Tree":
+                CurrentSong = CrossfadeInstance.WindmillBGM;
+                CurrentAmbient = CrossfadeInstance.WindmillAmbient;
+                break;
+            case "Market":
+                CurrentSong = CrossfadeInstance.MarketBGM;
+                CurrentAmbient = CrossfadeInstance.MarketAmbient;
+                break;
+            default:
+                CurrentSong = null;
+                CurrentAmbient = null;
+                break;
+        }
+        
+        // 1f will need to be changed to match the final volume used
+        while (!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            CurrentSong.volume = 1f - fTimeCounter;
+            CurrentAmbient.volume = 1f - fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }
 
-		switch (Current)
-		{
-			case "Forest":
-				CurrentSong = CrossfadeInstance.ForestBGM;
-				break;
-			case "Beach":
-				CurrentSong = CrossfadeInstance.BeachBGM;
-				break;
-			case "Cliff":
-				CurrentSong = CrossfadeInstance.CliffBGM;
-				break;
-			case "Light":
-				CurrentSong = CrossfadeInstance.LighthouseBGM;
-				break;
-			case "Tree":
-				CurrentSong = CrossfadeInstance.TreeBGM;
-				break;
-			case "Market":
-				CurrentSong = CrossfadeInstance.MarketBGM;
-				break;
-			default:
-				CurrentSong = null;
-				break;
-		}
+        CurrentSong.Stop();
+        CurrentAmbient.Stop();
 
-		switch (Next)
-		{
-			case "Forest":
-				NextSong = CrossfadeInstance.ForestBGM;
-				break;
-			case "Beach":
-				NextSong = CrossfadeInstance.BeachBGM;
-				break;
-			case "Cliff":
-				NextSong = CrossfadeInstance.CliffBGM;
-				break;
-			case "Light":
-				NextSong = CrossfadeInstance.LighthouseBGM;
-				break;
-			case "Tree":
-				NextSong = CrossfadeInstance.TreeBGM;
-				break;
-			case "Market":
-				NextSong = CrossfadeInstance.MarketBGM;
-				break;
-			default:
-				NextSong = null;
-				break;
-		}
+        Crossfade.CrossfadeInstance.StopCoroutine("CoroutineFadeDown");
+    }
 
-		float fTimeCounter = 0f;
+    public static IEnumerator CoroutineFadeUp(string Current)
+    {
+        AudioSource CurrentSong;
+        AudioSource CurrentAmbient;
+        float fTimeCounter = 0f;
 
-		NextSong.volume = 0;
-		NextSong.Play();
-		
-		while(!(Mathf.Approximately(fTimeCounter, 1f)))
-		{
-			fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
-			CurrentSong.volume = 1f - fTimeCounter;
-			NextSong.volume = fTimeCounter;
-			yield return new WaitForSeconds(0.02f);
-		}
-		
-		CurrentSong.Stop();
-		
-		
-		Crossfade.CrossfadeInstance.StopCoroutine("CoroutineFadeOverTime");
-	}
+        switch (Current)
+        {
+            case "Forest":
+                CurrentSong = CrossfadeInstance.ForestBGM;
+                CurrentAmbient = CrossfadeInstance.ForestAmbient;
+                break;
+            case "Beach":
+                CurrentSong = CrossfadeInstance.BeachBGM;
+                CurrentAmbient = CrossfadeInstance.BeachAmbient;
+                break;
+            case "Cliff":
+                CurrentSong = CrossfadeInstance.FarmBGM;
+                CurrentAmbient = CrossfadeInstance.FarmAmbient;
+                break;
+            case "Light":
+                CurrentSong = CrossfadeInstance.LighthouseBGM;
+                CurrentAmbient = CrossfadeInstance.LighthouseAmbient;
+                break;
+            case "Tree":
+                CurrentSong = CrossfadeInstance.WindmillBGM;
+                CurrentAmbient = CrossfadeInstance.WindmillAmbient;
+                break;
+            case "Market":
+                CurrentSong = CrossfadeInstance.MarketBGM;
+                CurrentAmbient = CrossfadeInstance.MarketAmbient;
+                break;
+            default:
+                CurrentSong = null;
+                CurrentAmbient = null;
+                break;
+        }
+
+        CurrentSong.volume = 0;
+        CurrentSong.Play();
+        CurrentAmbient.volume = 0;
+        CurrentAmbient.Play();
+
+        // 1f will need to be changed to match the final volume used
+        while (!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            CurrentSong.volume = fTimeCounter;
+            CurrentAmbient.volume = fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        Crossfade.CrossfadeInstance.StopCoroutine("CoroutineFadeUp");
+    }
+
+    public static IEnumerator CoroutineFadeOverTime(string Current, string Next)
+    {
+
+        AudioSource CurrentSong;
+        AudioSource CurrentAmbient;
+        AudioSource NextSong;
+        AudioSource NextAmbient;
+
+        switch (Current)
+        {
+            case "Forest":
+                CurrentSong = CrossfadeInstance.ForestBGM;
+                CurrentAmbient = CrossfadeInstance.ForestAmbient;
+                break;
+            case "Beach":
+                CurrentSong = CrossfadeInstance.BeachBGM;
+                CurrentAmbient = CrossfadeInstance.BeachAmbient;
+                break;
+            case "Cliff":
+                CurrentSong = CrossfadeInstance.FarmBGM;
+                CurrentAmbient = CrossfadeInstance.FarmAmbient;
+                break;
+            case "Light":
+                CurrentSong = CrossfadeInstance.LighthouseBGM;
+                CurrentAmbient = CrossfadeInstance.LighthouseAmbient;
+                break;
+            case "Tree":
+                CurrentSong = CrossfadeInstance.WindmillBGM;
+                CurrentAmbient = CrossfadeInstance.WindmillAmbient;
+                break;
+            case "Market":
+                CurrentSong = CrossfadeInstance.MarketBGM;
+                CurrentAmbient = CrossfadeInstance.MarketAmbient;
+                break;
+            default:
+                CurrentSong = null;
+                CurrentAmbient = null;
+                break;
+        }
+
+        switch (Next)
+        {
+            case "Forest":
+                NextSong = CrossfadeInstance.ForestBGM;
+                NextAmbient = CrossfadeInstance.ForestAmbient;
+                break;
+            case "Beach":
+                NextSong = CrossfadeInstance.BeachBGM;
+                NextAmbient = CrossfadeInstance.BeachAmbient;
+                break;
+            case "Cliff":
+                NextSong = CrossfadeInstance.FarmBGM;
+                NextAmbient = CrossfadeInstance.FarmAmbient;
+                break;
+            case "Light":
+                NextSong = CrossfadeInstance.LighthouseBGM;
+                NextAmbient = CrossfadeInstance.LighthouseAmbient;
+                break;
+            case "Tree":
+                NextSong = CrossfadeInstance.WindmillBGM;
+                NextAmbient = CrossfadeInstance.WindmillAmbient;
+                break;
+            case "Market":
+                NextSong = CrossfadeInstance.MarketBGM;
+                NextAmbient = CrossfadeInstance.MarketAmbient;
+                break;
+            default:
+                NextSong = null;
+                NextAmbient = null;
+                break;
+        }
+
+        float fTimeCounter = 0f;
+
+        NextSong.volume = 0;
+        NextSong.Play();
+        NextAmbient.volume = 0;
+        NextAmbient.Play();
+        
+        while(!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            CurrentSong.volume = 1f - fTimeCounter;
+            CurrentAmbient.volume = 1f - fTimeCounter;
+            NextSong.volume = fTimeCounter;
+            NextAmbient.volume = fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        /*while (!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            NextSong.volume = fTimeCounter;
+            NextAmbient.volume = fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }*/
+        
+        CurrentSong.Stop();
+        CurrentAmbient.Stop();
+        
+        Crossfade.CrossfadeInstance.StopCoroutine("CoroutineFadeOverTime");
+    }
 }
--- assets/Scripts/Cheats/CheatListener.cs
+++ assets/Scripts/Cheats/CheatListener.cs
@@ -2,108 +2,112 @@ using UnityEngine;
 using System.Collections;
 
 public class CheatListener : MonoBehaviour {
-	public bool isFlying = false;
-	public bool isFastMovement = false;
-	public bool isNoClip = false;
-	public bool isSoundOff = true;
-	
-	public float speedIncrease = 2;
-	
-	protected Player player;
-	protected LevelManager levelManager;
-	protected CharacterController characterController;
-	
-	private float currentHorizontalSpeed = 0.0f;
-	
-	void Start() {
-		player = GameObject.Find("PlayerCharacter").GetComponent<Player>();
-		levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
-		characterController = GameObject.Find("PlayerCharacter").GetComponent<CharacterController>();
-	}
-	
-	void Update() {
-		Fly();
-		MoveFast();
-		ToggleSound();
-		KeyboardMovement();
-	}
-	
-	void KeyboardMovement(){
-		float verticalMovement = Input.GetAxisRaw(Strings.ButtonVertical);
-		float horizontalMovement = Input.GetAxisRaw(Strings.ButtonHorizontal);	
-		
-		currentHorizontalSpeed = player.walkSpeed * horizontalMovement;
-		
-		if(!player.isAffectedByGravity){
-			player.currentVerticalSpeed = player.walkSpeed * verticalMovement;	
-		}
-		
-		MovePlayer();
-	}
-	void MovePlayer(){
-		// Calculate actual motion
-		Vector3 movement = new Vector3(currentHorizontalSpeed, player.currentVerticalSpeed, 0 );
-		movement *= Time.deltaTime;
-		
-		player.lastReturnedCollisionFlags = characterController.Move(movement);
-	}
-	
-	void Fly() {
-		if (isFlying){
-			player.isAffectedByGravity = false;
-		}
-		
-		if (Input.GetKeyDown(KeyCode.F)) {
-			isFlying = !isFlying;
-			
-			if (!isFlying) {
-				player.isAffectedByGravity = true;
-			}
-		}
-	}
-	
-	void MoveFast() {		
-		if (Input.GetKeyDown(KeyCode.M)) {
-			isFastMovement = !isFastMovement;
-			
-			if (isFastMovement) {
-				player.walkSpeed = player.walkSpeed * speedIncrease;
-			} else {
-				player.walkSpeed = player.walkSpeed / speedIncrease;	
-			}
-		}	
-	}
-	
-	void NoClip() {		// NOTE DOES NOT WORK
-		if (Input.GetKeyDown(KeyCode.N)) {
-			isNoClip = !isNoClip;
-			
-			if (isNoClip) {
-				characterController.detectCollisions = false;
-			} else {
-				characterController.detectCollisions = true;	
-			}
-		}	
-	}
-	
-	void ToggleSound() {
-		if (Input.GetKeyDown(KeyCode.A))
-		{
-			isSoundOff = !isSoundOff;
+    public bool isFlying = false;
+    public bool isFastMovement = false;
+    public bool isNoClip = false;
+    public bool isSoundOff = true;
+    
+    public float speedIncrease = 2;
+    
+    protected Player player;
+    protected LevelManager levelManager;
+    protected CharacterController characterController;
+    
+    private float currentHorizontalSpeed = 0.0f;
+    
+    void Start() {
+        player = GameObject.Find("PlayerCharacter").GetComponent<Player>();
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        characterController = GameObject.Find("PlayerCharacter").GetComponent<CharacterController>();
+    }
+    
+    void Update() {
+        Fly();
+        MoveFast();
+        ToggleSound();
+        KeyboardMovement();
+    }
+    
+    void KeyboardMovement(){
+        float verticalMovement = Input.GetAxisRaw(Strings.ButtonVertical);
+        float horizontalMovement = Input.GetAxisRaw(Strings.ButtonHorizontal);	
+        
+        currentHorizontalSpeed = player.walkSpeed * horizontalMovement;
+        
+        if(!player.isAffectedByGravity){
+            player.currentVerticalSpeed = player.walkSpeed * verticalMovement;	
+        }
+        
+        MovePlayer();
+    }
+    void MovePlayer(){
+        // Calculate actual motion
+        Vector3 movement = new Vector3(currentHorizontalSpeed, player.currentVerticalSpeed, 0 );
+        movement *= Time.deltaTime;
+        
+        player.lastReturnedCollisionFlags = characterController.Move(movement);
+    }
+    
+    void Fly() {
+        if (isFlying){
+            player.isAffectedByGravity = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F)) {
+            isFlying = !isFlying;
+            
+            if (!isFlying) {
+                player.isAffectedByGravity = true;
+            }
+        }
+    }
+    
+    void MoveFast() {		
+        if (Input.GetKeyDown(KeyCode.M)) {
+            isFastMovement = !isFastMovement;
+            
+            if (isFastMovement) {
+                player.walkSpeed = player.walkSpeed * speedIncrease;
+            } else {
+                player.walkSpeed = player.walkSpeed / speedIncrease;	
+            }
+        }	
+    }
+    
+    void NoClip() {		// NOTE DOES NOT WORK
+        if (Input.GetKeyDown(KeyCode.N)) {
+            isNoClip = !isNoClip;
+            
+            if (isNoClip) {
+                characterController.detectCollisions = false;
+            } else {
+                characterController.detectCollisions = true;	
+            }
+        }	
+    }
+    
+    void ToggleSound() {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isSoundOff = !isSoundOff;
 
-			if (isSoundOff)
-			{
-				AudioListener.pause = true;
-			}
-			else
-			{
-				AudioListener.pause = false;
-			}
-		}
+            if (isSoundOff)
+            {
+                AudioListener.pause = true;
+            }
+            else
+            {
+                AudioListener.pause = false;
+            }
+        }
 
-		if (Input.GetKeyDown(KeyCode.Z))
-		{
-			StartCoroutine(Crossfade.CoroutineFadeOverTime("Forest", "Beach"));
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Crossfade.CoroutineFadeOverTime("Forest", "Beach"));
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(Crossfade.CoroutineFadeOverTime("Beach", "Forest"));
+        }
+    }
 }
