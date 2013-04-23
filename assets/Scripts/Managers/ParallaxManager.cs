ee8f86684ca7b5055112626fc965622bc584b72b
ElBathroom <wadehribar@gmail.com>
2013-04-23 16:56:23 -0700

    Added in parallaxing. Base scene now holds ParallaxManager.cs, there is now a "Parallax" tag, and 4 parallax levels.

---
+++ assets/Scripts/Managers/ParallaxManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParallaxManager : MonoBehaviour {
+	public float speedOne = 2.5F;
+	public float speedTwo = 2.0F;
+	public float speedThree = 1.5F;
+	public float speedFour = 1.2F;
+	private Player player;
+	private Vector3 prevPos;
+	private Vector3 deltaPos;
+	public Vector3 DeltaPos {
+		get { return deltaPos; }
+	}
+	private GameObject[] parallaxObjects;
+	
+	void Start () {
+		
+	}
+	
+	public void Init() {
+		player = GameObject.Find("PlayerCharacter").GetComponent<Player>();
+		prevPos = player.transform.position;
+        parallaxObjects = GameObject.FindGameObjectsWithTag(Strings.PARALLAX);
+	}
+	
+	void Update () {
+		if (parallaxObjects != null) {
+			deltaPos = prevPos - player.transform.position;
+			prevPos = player.transform.position;
+			foreach (GameObject obj in parallaxObjects) {
+				switch(LayerMask.LayerToName(obj.layer)) {
+					case Strings.PARALLAXONE:
+						moveObject(speedOne, obj);
+						break;
+					case Strings.PARALLAXTWO:
+						moveObject(speedTwo, obj);
+						break;
+					case Strings.PARALLAXTHREE:
+						moveObject(speedThree, obj);
+						break;
+					case Strings.PARALLAXFOUR:
+						moveObject(speedFour, obj);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+	
+	private void moveObject(float speed, GameObject obj) {
+		Vector3 newPos = obj.transform.position;
+		newPos.x += deltaPos.x * speed;
+		obj.transform.position = newPos;
+	}
+}
