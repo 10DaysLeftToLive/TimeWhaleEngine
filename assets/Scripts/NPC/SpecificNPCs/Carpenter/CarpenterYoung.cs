749afa915f0f82ec4d07b398855e3e421b6e6f6e
Jared Mavis <jmavis@ucsc.edu>
2013-04-23 20:33:45 -0700

    Moar cleanup and added in Carpenter and son.

---
+++ assets/Scripts/NPC/SpecificNPCs/Carpenter/CarpenterYoung.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Carpenter young specific scripting values
+/// </summary>
+public class CarpenterYoung : NPC {
+	protected override void Init() {
+		base.Init();
+	}
+	
+	protected override void SetFlagReactions(){
+		
+	}
+	
+	protected override EmotionState GetInitEmotionState(){
+		return (new InitialEmotionState(this, "Hai"));
+	}
+	
+	protected override Schedule GetSchedule(){
+		Schedule schedule = new Schedule(this);
+		
+		Task standAround = new Task(new IdleState(this));
+		
+		schedule.Add(standAround);
+		
+		return (schedule);
+	}
+	
+	#region EmotionStates
+		#region Initial Emotion State
+		private class InitialEmotionState : EmotionState{
+			public InitialEmotionState(NPC toControl, string currentDialogue) : base(toControl, currentDialogue){
+				
+			}
+			
+			public override void UpdateEmotionState(){
+				
+			}
+		}
+		#endregion
+	#endregion
+}
