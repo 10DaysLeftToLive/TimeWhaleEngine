d24966ca2c3a3a632776b9b0b6c2a55ea5631771
Jared Mavis <jmavis@ucsc.edu>
2013-04-23 23:19:20 -0700

    Frog doesn't get back up.

--- assets/Scripts/MechanicsScripts/Shenanigans/StepOn.cs
+++ assets/Scripts/MechanicsScripts/Shenanigans/StepOn.cs
@@ -3,12 +3,12 @@ using SmoothMoves;
 using System.Collections;
 
 public class StepOn : MonoBehaviour {
+	bool wasStepedOn = false;
 	void OnTriggerEnter(Collider collider) {
-		Debug.Log("FROGIAMDEAD!");
-		if (collider.gameObject.name == Strings.Player) {
+		if (collider.gameObject.name == Strings.Player && !wasStepedOn) {
 			this.GetComponent<SmoothMoves.BoneAnimation>().Play("Explode");
 			FlagManager.instance.SetFlag(FlagStrings.CrushFrog);
-			//this.GetComponent<SmoothMoves.Sprite>().atlas.material = squashed;
+			wasStepedOn = true;
 		}
 	}
 }
