3347ebbb799485a881fc0c7553074d18e712c369
Jared Mavis <jmavis@ucsc.edu>
2013-04-26 15:58:13 -0700

    Weird error

--- assets/Scripts/NPC/Reactions/InteractionActions/UpdateDefaultTextAction.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class UpdateDefaultTextAction : InteractionUpdateAction {
-	public override void Perform(){
-		GUIManager.Instance.UpdateInteractionDisplay(newText);
-	}
-}
