491b465ed601030378c1920fd4ba6a8bc4d53228
bsnguyen <nguyen.brian475@gmail.com>
2013-05-01 14:31:32 -0700

    Added in farmer family and lighthouse girl temps, added them to young scene along with NPC code

---
+++ assets/scripts/NPC/SpecificNPCs/FarmerFather/FarmerFatherYoung.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Farmer Father young specific scripting values
+/// </summary>
+public class FarmerFatherYoung : NPC {	
+	protected override void Init() {
+		base.Init();
+	}
+	
+	protected override void SetFlagReactions(){
+		
+	}
+	
+	protected override EmotionState GetInitEmotionState(){
+		return (new InitialEmotionState(this, "||||PUT IT UP PUT IT UP YO"));
+	}
+	
+	protected override Schedule GetSchedule(){
+		Schedule schedule = new DefaultSchedule(this);
+		return (schedule);
+	}
+
+	protected override void SetUpSchedules(){
+		
+	}
+	
+	
+	#region EmotionStates
+	#region Initial Emotion State
+	private class InitialEmotionState : EmotionState{
+	
+	
+		public InitialEmotionState(NPC toControl, string currentDialogue) : base(toControl, currentDialogue){
+			
+		
+		}
+		
+		public override void UpdateEmotionState(){
+			
+		}
+	
+	}
+	#endregion
+	#endregion
+}
---
+++ assets/scripts/NPC/SpecificNPCs/FarmerMother/FarmerMotherYoung.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Farmer Mother young specific scripting values
+/// </summary>
+public class FarmerMotherYoung : NPC {	
+	protected override void Init() {
+		base.Init();
+	}
+	
+	protected override void SetFlagReactions(){
+		
+	}
+	
+	protected override EmotionState GetInitEmotionState(){
+		return (new InitialEmotionState(this, "||||ALL RIGHT ALL RIGHT ALL RIIIIIGHT"));
+	}
+	
+	protected override Schedule GetSchedule(){
+		Schedule schedule = new DefaultSchedule(this);
+		return (schedule);
+	}
+
+	protected override void SetUpSchedules(){
+		
+	}
+	
+	
+	#region EmotionStates
+	#region Initial Emotion State
+	private class InitialEmotionState : EmotionState{
+	
+	
+		public InitialEmotionState(NPC toControl, string currentDialogue) : base(toControl, currentDialogue){
+			
+		
+		}
+		
+		public override void UpdateEmotionState(){
+			
+		}
+	
+	}
+	#endregion
+	#endregion
+}
