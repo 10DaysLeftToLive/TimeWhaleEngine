d3c107ccfe179a12c245cc5d9a2af5120ae7451e
Jared Mavis <jmavis@ucsc.edu>
2013-05-12 16:14:41 -0700

    Changing the chat menu for easier altering.

--- assets/Scripts/GUI/GUIControls/ChatMenu.cs
+++ assets/Scripts/GUI/GUIControls/ChatMenu.cs
@@ -13,6 +13,10 @@ public class ChatMenu : GUIControl {
 	private static float CHATWIDTH = .2f;
 	private static float CHATHEIGHT = .2f;
 	
+	public float distAboveHead = 34f;
+	public float distToRightOfHead = -3.7f;
+	public GUIStyle chatBoxStyle;
+	
 	public override void Init(){
 		_npcsChats = new List<NPCChat>();
 		_currentChats = new List<ChatInfo>();
@@ -39,26 +43,32 @@ public class ChatMenu : GUIControl {
 	}
 			
 	public void UpdateChat(ChatInfo oldChat, ChatInfo newChat){
-		Debug.Log("Updating chat (" + oldChat.text + ", " + newChat.text + ")");
 		_currentChats.Remove(oldChat);
 		_currentChats.Add(newChat);
 	}
 	
+	private bool hasSetStyle = false;
 	public override void Render(){
+		if (hasSetStyle){
+			hasSetStyle = !hasSetStyle;
+		}
 		ShowChats();
 	}
 	
 	private void ShowChats(){
 		foreach (ChatInfo chatInfo in _currentChats){
-			GUI.Label(GetRectOverNPC(chatInfo.npcTalking), chatInfo.text);	
+			GUI.Label(GetRectOverNPC(chatInfo.npcTalking), chatInfo.text, chatBoxStyle);	
 		}
 	}
 	
 	private Rect GetRectOverNPC(NPC npc){
 		Vector3 pos = npc.transform.position;
+		pos.y = npc.transform.collider.bounds.max.y;
+		pos.x += npc.transform.collider.bounds.size.x * distToRightOfHead;
 		Vector2 screenPos = Camera.mainCamera.WorldToScreenPoint(pos);
+		screenPos.y += distAboveHead;
 		// invert the y and put it above the npc, kinda arbitrary values
-		Vector2 percentageConvertedPos = new Vector2(screenPos.x/ScreenSetup.screenWidth - .1f, 1 - (screenPos.y/ScreenSetup.screenHeight) - .2f);
+		Vector2 percentageConvertedPos = new Vector2(screenPos.x/ScreenSetup.screenWidth, 1 - (screenPos.y/ScreenSetup.screenHeight));
 		return (ScreenRectangle.NewRect(percentageConvertedPos.x, percentageConvertedPos.y, CHATWIDTH, CHATHEIGHT));
 	}
 }
