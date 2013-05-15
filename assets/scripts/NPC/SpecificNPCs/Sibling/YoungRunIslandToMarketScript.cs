50e74a87d314536c7ef49496183a583d42f36b3b
Kevin Otoshi <kevinotoshi@gmail.com>
2013-05-14 18:34:58 -0700

    adding missing scripts, youngRunMarketScript, youngRunReflectionTreeScript, youngRunHomeScript

---
+++ assets/scripts/NPC/SpecificNPCs/Sibling/YoungRunIslandToMarketScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class YoungRunIslandToMarketScript : Schedule {
+	
+	public YoungRunIslandToMarketScript (NPC toManage) : base (toManage) {
+		
+		schedulePriority = (int)priorityEnum.Low;			
+	}
+	
+	protected override void Init() {
+		Add(new TimeTask(.2f, new IdleState(_toManage))); //or self-triggering
+		Add(new Task(new MoveThenDoState(_toManage, new Vector3 (33f, 7.5f, .3f), new MarkTaskDone(_toManage))));
+		Add(new TimeTask(10f, new WaitTillPlayerCloseState(_toManage, _toManage.player)));
+		Add(new TimeTask(5f, new IdleState(_toManage)));
+		Task setOffWindmillFlag = new Task((new MoveThenDoState(_toManage, new Vector3 (31f, 7.5f,.3f), new MarkTaskDone(_toManage))));
+		setOffWindmillFlag.AddFlagToSet(FlagStrings.RunToWindmill);
+		Add(setOffWindmillFlag);
+		Add(new TimeTask(.2f, new IdleState(_toManage)));
+	}
+}
