b4e49dc6a5106e46ae6fe3616012d50c738d9ee1
ElBathroom <wadehribar@yahoo.com>
2013-05-21 17:13:08 -0700

    NPCs can now lead the player to a spot using the LeadPlayerToThenDoState. When the player gets too far away, the NPC will stop and yell at the player to come back.

---
+++ assets/scripts/Character/States/MovementStates/LeadPlayerToThenDoState.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class LeadPlayerToThenDoState : MoveThenDoState {
+	private Player _player;
+	private ShowOneOffChatAction waitYell;
+	private float timeToYellAgain;
+	private static float NEAR_PLAYER = 6f;
+	private static float TIME_TO_YELL_AGAIN = 5f;
+	
+	public LeadPlayerToThenDoState(Character toControl, Player player, Vector3 goal) : base(toControl, goal, new MarkTaskDone(toControl)){
+		_player = player;
+		if (toControl is NPC) {
+			waitYell = new ShowOneOffChatAction((NPC)toControl, "Come back. I'm leading the way.");
+		} else {
+			Debug.LogWarning("Player trying to use LeadPlayerToThenDoState");
+		}
+	}
+	
+	public override void Update () {
+		timeToYellAgain -= Time.deltaTime;
+		if (Utils.InDistance(character.gameObject, _player.gameObject, NEAR_PLAYER)) {
+			base.Update();
+		} else {
+			character.PlayAnimation(Strings.animation_stand);
+			if (timeToYellAgain <= 0) {
+				waitYell.Perform();
+				timeToYellAgain = TIME_TO_YELL_AGAIN;
+			}
+		}
+	}
+}
