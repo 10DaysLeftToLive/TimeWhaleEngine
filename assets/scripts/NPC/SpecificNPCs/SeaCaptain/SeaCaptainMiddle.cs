e6de0c557ce5e36771dea8910c72d5a1c12fc976
ElBathroom <wadehribar@yahoo.com>
2013-05-27 03:30:29 -0700

    Basic interactions for the sea captain

--- assets/scripts/NPC/SpecificNPCs/SeaCaptain/SeaCaptainMiddle.cs
+++ assets/scripts/NPC/SpecificNPCs/SeaCaptain/SeaCaptainMiddle.cs
@@ -20,7 +20,7 @@ public class SeaCaptainMiddle : NPC {
 	}
 	
 	protected override EmotionState GetInitEmotionState(){
-		return (new InitialEmotionState(this, "Ooooy! Good winds we got fer sailing. A pity me ship is in disrepair, but in the meantime there's treasure to be found!"));
+		return (new InitialEmotionState(this, "Ahoy laddie! I am the legendeary sea captain of the six seas, or was it 7..."));
 	}
 	
 	protected override Schedule GetSchedule(){
@@ -36,16 +36,74 @@ public class SeaCaptainMiddle : NPC {
 	#region EmotionStates
 	#region Initial Emotion State
 	private class InitialEmotionState : EmotionState{
-	
+		Choice whyHereChoice;
+		Reaction whyHereReaction;
+		Choice whereShipChoice;
+		Reaction whereShipReaction;
+		
+		Reaction appleReaction;
+		Reaction applePieReaction;
+		Reaction veggiesReaction;
+		Reaction shovelReaction;
+		Reaction toyShipReaction;
 	
 		public InitialEmotionState(NPC toControl, string currentDialogue) : base(toControl, currentDialogue){
+			#region item reactions
+			Reaction appleReaction = new Reaction();
+			appleReaction.AddAction(new NPCTakeItemAction(toControl));
+			appleReaction.AddAction(new UpdateCurrentTextAction(toControl, "This reminds me of when I stole an apple from the royal gardens. I barely escaped with my life!"));
+			_allItemReactions.Add(StringsItem.Apple,  new DispositionDependentReaction(appleReaction));
 			
-		
+			Reaction applePieReaction = new Reaction();
+			applePieReaction.AddAction(new NPCTakeItemAction(toControl));
+			applePieReaction.AddAction(new UpdateCurrentTextAction(toControl, "I haven't eaten applie pie this good, since I impersonated a noble to eat at the Sultan's palace!"));
+			_allItemReactions.Add(StringsItem.ApplePie,  new DispositionDependentReaction(applePieReaction));
+			
+			Reaction veggiesReaction = new Reaction();
+			veggiesReaction.AddAction(new NPCTakeItemAction(toControl));
+			veggiesReaction.AddAction(new UpdateCurrentTextAction(toControl, "I remember eating a strange root when I was stranded on a deserted island. It made me think that I could drink sea water."));
+			_allItemReactions.Add(StringsItem.Vegetable,  new DispositionDependentReaction(veggiesReaction));
+			
+			Reaction shovelReaction = new Reaction();
+			shovelReaction.AddAction(new NPCTakeItemAction(toControl));
+			shovelReaction.AddAction(new UpdateCurrentTextAction(toControl, "Thanks laddie! I can now go and dig for treasure."));
+			_allItemReactions.Add(StringsItem.Shovel,  new DispositionDependentReaction(shovelReaction));
+			
+			Reaction toyShipReaction = new Reaction();
+			toyShipReaction.AddAction(new NPCTakeItemAction(toControl));
+			toyShipReaction.AddAction(new UpdateCurrentTextAction(toControl, "Ahhh... what a grand vessel! Reminds me of me own ship. Thanks laddie!"));
+			_allItemReactions.Add(StringsItem.ToyBoat,  new DispositionDependentReaction(toyShipReaction));
+			#endregion
+			
+			#region choices
+			whyHereChoice = new Choice("Why are you here?", "I am searching for great treasure said to be buried on this island, but I can not figure out what the map is saying.");
+			whyHereReaction = new Reaction();
+			whyHereReaction.AddAction(new NPCCallbackAction(UpdateWhyHere));
+			whyHereReaction.AddAction(new UpdateCurrentTextAction(toControl, "How 'ave ye been laddie?"));
+			_allChoiceReactions.Add(whyHereChoice, new DispositionDependentReaction(whyHereReaction));
+			
+			whereShipChoice = new Choice("Where is your ship?", "Well... I might have forgot to drop the anchor before commin' ashore.");
+			whereShipReaction = new Reaction();
+			whereShipReaction.AddAction(new NPCCallbackAction(UpdateWhereShip));
+			whereShipReaction.AddAction(new UpdateCurrentTextAction(toControl, "How 'ave ye been laddie?"));
+			_allChoiceReactions.Add(whereShipChoice, new DispositionDependentReaction(whereShipReaction));
+			#endregion
+			
 		}
 		
 		public override void UpdateEmotionState(){
 			
 		}
+		
+		#region update methods
+		public void UpdateWhyHere(){
+			GUIManager.Instance.RefreshInteraction();
+		}
+		
+		public void UpdateWhereShip(){
+			GUIManager.Instance.RefreshInteraction();
+		}
+		#endregion
 	
 	}
 	#endregion
