7214be539fc04267581b0f7eb4f528a2116a8a6b
esawin <esawin@ucsc.edu>
2013-06-02 16:21:42 -0700

    redid dating conversation using schedules

---
+++ Assets/Scripts/Utility/FarmerFamilyFlagStrings.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public static class FarmerFamilyFlagStrings {
+	
+	public static string GirlCastleDateOne = "Girl to castle date one";
+	public static string GirlCastleDateTwo = "Girl to castle date two";
+	public static string GirlCastleDateThree = "Girl to castle date three";
+	public static string GirlCastleDateFour = "Girl to castle date four";
+	public static string GirlCastleDateFive = "Girl to castle date five";
+	public static string GirlCastleDateSix = "Girl to castle date six";
+}
+
