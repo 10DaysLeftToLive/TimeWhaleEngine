4b27a0c54fabd1aee89a3f9eda9e2a6fd07ffc41
Jared Mavis <jmavis@ucsc.edu>
2013-06-05 12:34:35 -0700

    Can check if a flag has been set now.

--- assets/Scripts/FlagSystem/FlagManager.cs
+++ assets/Scripts/FlagSystem/FlagManager.cs
@@ -42,6 +42,15 @@ public class FlagManager : MonoBehaviour {
 		DebugManager.instance.Log("Flag " + name + " was not found", "Flag", "Warning");
 	}
 	
+	public bool FlagIsSet(string flagName){
+		foreach (Flag flag in _flags){
+			if (flag.Equals(flagName)){
+				return (flag._isSetOff);
+			}
+		}
+		return (false);
+	}
+	
 	public void UnSetFlag(string name){
 		foreach (Flag flag in _flags){
 			if (flag.Equals(name)){
